265cc3d128e9521bec6e5ca8bf610dfbc4d8b70f
Bogdan Yaroshchuk <yaroshchuk.b.2092@gmail.com>
2022-10-23 13:01:59 +0300

    feat: adde global styles, routes, changed app name

--- frontend/book-library/src/app/app-routing.module.ts
+++ frontend/book-library/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+import { BookListComponent } from './main-display/book-list/book-list.component';
+import { MainDisplayComponent } from './main-display/main-display.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibraryComponent } from './main-display/library/library.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BookListComponent,
+  },
+  {
+    path: 'library',
+    component: LibraryComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/book-library/src/app/app.module.ts
+++ frontend/book-library/src/app/app.module.ts
@@ -1,20 +1,36 @@
+import { BookCardComponent } from './componets/book-card/book-card.component';
+import { BookListComponent } from './main-display/book-list/book-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiService } from './services/api.service';
+import { AppComponent } from './app.component';
+import { MainDisplayComponent } from './main-display/main-display.component';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainDisplayComponent } from './main-display/main-display.component';
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { PieChartOutline } from '@ant-design/icons-angular/icons';
+import { UnorderedListOutline } from '@ant-design/icons-angular/icons';
+import { LibraryComponent } from './main-display/library/library.component';
+const icons: IconDefinition[] = [PieChartOutline, UnorderedListOutline];
 @NgModule({
   declarations: [
     AppComponent,
-    MainDisplayComponent
+    MainDisplayComponent,
+    BookCardComponent,
+    BookListComponent,
+    LibraryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzLayoutModule,
+    NzIconModule.forChild(icons),
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, ApiService, HttpClientModule],
 })
-export class AppModule { }
+export class AppModule {}
