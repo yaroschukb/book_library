849fa9b26d9bccbc78028b96fc01feadc815cdd6
Bogdan Yaroshchuk <yaroshchuk.b.2092@gmail.com>
2022-11-11 01:33:41 +0200

    feat: added empty-list and loader components

---
+++ frontend/book-library/src/app/componets/empty-list/empty-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-list',
+  templateUrl: './empty-list.component.html',
+  styleUrls: ['./empty-list.component.scss'],
+})
+export class EmptyListComponent implements OnInit {
+  @Input() text: string = '';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
