bd951e08cf71e0200c1f1fea4097997578881470
Bogdan Yaroshchuk <yaroshchuk.b.2092@gmail.com>
2022-11-11 01:43:04 +0200

    feat: updated routes

--- frontend/book-library/src/app/app-routing.module.ts
+++ frontend/book-library/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './main-display/home/home.component';
 import { BookListComponent } from './main-display/book-list/book-list.component';
 import { MainDisplayComponent } from './main-display/main-display.component';
 import { NgModule } from '@angular/core';
@@ -5,6 +6,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LibraryComponent } from './main-display/library/library.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
   {
     path: 'books',
     component: BookListComponent,
