b1b958528573c8ba109db85e98da0cf4530e7943
Bogdan Yaroshchuk <yaroshchuk.b.2092@gmail.com>
2022-11-21 02:47:46 +0200

    feat: added styles, updated template, added deleteCard function

--- frontend/book-library/src/app/componets/book-card/book-card.component.ts
+++ frontend/book-library/src/app/componets/book-card/book-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from './../../services/api.service';
+import { Book } from './../../interfaces/book';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-book-card',
@@ -6,10 +8,20 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./book-card.component.scss'],
 })
 export class BookCardComponent implements OnInit {
-  @Input() author: any;
-  @Input() rate: any;
-  @Input() description: any;
-  constructor() {}
+  @Input() book: Book = {};
+  // @Input() editCard:(args:any)=>void;
+  // @Input() deleteCard();
+
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {}
+  editCard(book: Book) {
+    console.log('Edit', book);
+  }
+  deleteCard(book: Book) {
+    console.log('delete', book);
+    return this.api.deleteBook(book._id).subscribe((i) => {
+      console.log('d', i);
+    });
+  }
 }
