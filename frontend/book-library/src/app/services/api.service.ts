07b631f3652e5b6e46eba74b5e5355128f6368de
Bogdan Yaroshchuk <yaroshchuk.b.2092@gmail.com>
2022-11-21 03:01:20 +0200

    feat: updated getBooks function, added addBook and deleteBook functions, added Subject

--- frontend/book-library/src/app/services/api.service.ts
+++ frontend/book-library/src/app/services/api.service.ts
@@ -1,14 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Observable, Subject, tap } from 'rxjs';
+import { threadId } from 'worker_threads';
+import { Book } from '../interfaces/book';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
-
-  async getBooks(): Promise<any> {
-    return lastValueFrom(this.http.get('api/books'));
+  private _refreshList = new Subject<void>();
+  get refresh() {
+    return this._refreshList;
+  }
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>('api/books');
+  }
+  addBook(body: any): Observable<Book> {
+    return this.http.post<Book>('api/books', body).pipe(
+      tap(() => {
+        this.refresh.next();
+      })
+    );
+  }
+  deleteBook(id: any): Observable<any> {
+    console.log(id);
+    return this.http.delete<Book>(`api/books/${id}`).pipe(
+      tap(() => {
+        this.refresh.next();
+      })
+    );
   }
 }
