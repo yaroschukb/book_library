26671f794104ad40e6fc384364df63870ba5dc66
Bogdan Yaroshchuk <yaroshchuk.b.2092@gmail.com>
2022-11-21 02:46:20 +0200

    feat: added new routes and components

--- frontend/book-library/src/app/app-routing.module.ts
+++ frontend/book-library/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { AddingBookComponent } from './componets/adding-book/adding-book.component';
+import { PlannedComponent } from './main-display/planned/planned.component';
 import { HomeComponent } from './main-display/home/home.component';
-import { BookListComponent } from './main-display/book-list/book-list.component';
+import { ReadingComponent } from './main-display/reading/reading.component';
 import { MainDisplayComponent } from './main-display/main-display.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReadComponent } from './main-display/read/read.component';
 import { LibraryComponent } from './main-display/library/library.component';
 
 const routes: Routes = [
@@ -12,8 +15,20 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'books',
-    component: BookListComponent,
+    path: 'adding-book',
+    component: AddingBookComponent,
+  },
+  {
+    path: 'reading',
+    component: ReadingComponent,
+  },
+  {
+    path: 'planned',
+    component: PlannedComponent,
+  },
+  {
+    path: 'read',
+    component: ReadComponent,
   },
   {
     path: 'library',
--- frontend/book-library/src/app/app.module.ts
+++ frontend/book-library/src/app/app.module.ts
@@ -1,5 +1,6 @@
+import { AddingBookComponent } from './componets/adding-book/adding-book.component';
 import { BookCardComponent } from './componets/book-card/book-card.component';
-import { BookListComponent } from './main-display/book-list/book-list.component';
+import { ReadingComponent } from './main-display/reading/reading.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ApiService } from './services/api.service';
 import { AppComponent } from './app.component';
@@ -9,22 +10,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { LibraryComponent } from './main-display/library/library.component';
+import { ReadComponent } from './main-display/read/read.component';
 import { LoaderComponent } from './componets/loader/loader.component';
 import { EmptyListComponent } from './componets/empty-list/empty-list.component';
 import { HomeComponent } from './main-display/home/home.component';
+import { PlannedComponent } from './main-display/planned/planned.component';
+import { CategorySelectorComponent } from './componets/category-selector/category-selector.component';
+import { LibraryComponent } from './main-display/library/library.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     MainDisplayComponent,
     BookCardComponent,
-    BookListComponent,
-    LibraryComponent,
+    ReadingComponent,
+    ReadComponent,
     LoaderComponent,
     EmptyListComponent,
     HomeComponent,
+    PlannedComponent,
+    AddingBookComponent,
+    CategorySelectorComponent,
+    LibraryComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent, ApiService, HttpClientModule],
 })
