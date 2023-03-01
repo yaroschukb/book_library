a610368070da920145b65031d996e303f9fbf77f
Bogdan Yaroshchuk <yaroshchuk.b.2092@gmail.com>
2023-03-01 04:15:16 +0200

    feat: added JwtModule, PassportModule, At and Rt strategies

--- book-server/src/auth/auth.module.ts
+++ book-server/src/auth/auth.module.ts
@@ -1,3 +1,5 @@
+import { AtJwtStrategy } from './auth-strategies/atJwt.strategy';
+import { RtJwtStrategy } from './auth-strategies/rtJwt.strategy';
 import { UserModule } from './../user/user.module';
 import { Users } from './../entities/user.entity';
 
@@ -5,10 +7,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { Repository } from 'typeorm';
 @Module({
-  imports: [TypeOrmModule.forFeature([Users]), UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([Users]),
+    UserModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRY_TIME,
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, Repository, AtJwtStrategy, RtJwtStrategy],
+  exports: [
+    AuthService,
+    AtJwtStrategy,
+    RtJwtStrategy,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
 })
 export class AuthModule {}
