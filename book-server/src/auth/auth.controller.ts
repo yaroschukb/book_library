bfbd6110d1b06ce89cb26e1989285626087dc0ff
Bogdan Yaroshchuk <yaroshchuk.b.2092@gmail.com>
2023-03-01 04:18:29 +0200

    feat: added signIn, logout, refresh routes, added public decorator and guards

--- book-server/src/auth/auth.controller.ts
+++ book-server/src/auth/auth.controller.ts
@@ -1,28 +1,55 @@
+import { RtGuard } from './../common/guards/rt.guard';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credenrials.dto';
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
-import { Users } from 'src/entities/user.entity';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Tokens } from 'src/common/types/tokens.type';
+import { Public } from 'src/common/decorators';
+import { SignInCredentialsDto } from './dto/signIn-credentials.dto';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @Post('/signup')
+
+  @Public()
+  @Post('/local/signup')
   async singUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
-  ): Promise<Users> {
-    const user = await this.authService.signUp(authCredentialsDto);
-    if (user) {
-      // delete user.password;
-      // delete user.salt;
-      return user;
-    }
+  ): Promise<Tokens> {
+    return await this.authService.signUp(authCredentialsDto);
+    // if (user) {
+    //   // delete user.password;
+    //   // delete user.salt;
+    //   return user;
+    // }
+  }
+  @Public()
+  @Post('/local/signin')
+  async singIn(
+    @Body(ValidationPipe) signInCredentialsDto: SignInCredentialsDto,
+  ): Promise<Tokens> {
+    return this.authService.signIn(signInCredentialsDto);
   }
 
-  @Post('/signin')
-  async singIn(
-    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
-  ): Promise<Users> {
-    return this.authService.signIn(authCredentialsDto);
+  @Post('/logout')
+  async logout(@Req() req: Request) {
+    const user = req.user;
+    return this.authService.logout(user['id']);
+  }
+  @Public()
+  @UseGuards(RtGuard)
+  @Post('/refresh')
+  async refresh(@Req() req: Request) {
+    const user = req.user;
+    return this.authService.refresh(user['id'], user['refreshToken']);
   }
 }
 // delete password and salt!!!!
